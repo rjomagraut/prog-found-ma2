39d25d784b34ccf66e5c266fa05f6bb67d26b602
André Lekve <60402486+rjomagraut@users.noreply.github.com>
2020-03-15 19:32:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// question 1
+var petArray = ['cat', 'cow', 'dog'];
+// your answer for question 1 goes here
+
+// question 2
+console.log(petArray[1]);
+// your answer for question 2 goes here
+
+// question 3
+petArray.push('sheep');
+// your answer for question 3 goes here
+
+// question 4
+console.log(petArray.length);
+// your answer for question 4 goes here
+
+// question 5
+var catObject = {
+  name: 'Fluffy',
+  colour: 'white',
+  age: 5
+};
+// your answer for question 5 goes here
+
+// question 6
+console.log(catObject.age);
+// your answer for question 6 goes here
+
+// question 7
+var catArray = [
+  {
+    name: 'Fluffy',
+    colour: 'white',
+    age: 5
+  }
+];
+// your answer for question 7 goes here
+
+// question 8
+for (var i = 0; i < catArray.length; i++) {
+  console.log(catArray[i].name);
+  console.log(catArray[i].colour);
+  console.log(catArray[i].age);
+}
+// your answer for question 8 goes here
+
+// question 9
+function logToConsole(fruite) {
+  console.log(fruite);
+}
+// your answer for question 9 goes here
+logToConsole('donkey');
+// question 10
+
+// your answer for question 10 goes here
